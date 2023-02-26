a50d966a8e479ed7cb2e83c70f451ebae7382092
kurotaku <kuroiwa.taku@gmail.com>
2023-02-26 23:44:53 +0900

    devise設定+AdminUserモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, controllers: {
+    registrations: "admin_users/registrations",
+    sessions: "admin_users/sessions",
+    passwords: "admin_users/passwords",
+    confirmations: "admin_users/confirmations"
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ApplicationRecord.transaction do
+  ##########################
+  # ユーザー
+  ##########################
+  p '=== AdminUser ==='
+
+  admin_user = AdminUser.find_or_initialize_by(email: ENV.fetch('DEMO_USER_EMAIL', 'test@test.com'))
+  p admin_user
+  admin_user.password = 'password'
+  admin_user.skip_confirmation!
+  admin_user.save!
+end
