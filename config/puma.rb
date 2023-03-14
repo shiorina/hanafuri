604b2928079842b32f98b73de0078ed0528eb37c
kurotaku <kuroiwa.taku@gmail.com>
2023-03-15 02:23:13 +0900

    temp

--- config/puma.rb
+++ config/puma.rb
@@ -17,6 +17,9 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 #
 port ENV.fetch("PORT") { 3000 }
 
+# Set up socket location
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
